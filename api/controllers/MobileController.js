48066a0da830a2e36a7481cb4b16cbeb370193b9
Christopher Pappas <damassi.pappas@gmail.com>
2013-11-19 13:11:24 -0800

    Updating REST api

--- api/controllers/MobileController.js
+++ api/controllers/MobileController.js
@@ -13,23 +13,11 @@ module.exports = {
    */
   index: function (req, res) {
 
-    console.log( req.params)
-
     var gameId = req.param('id')
       , socket = req.socket
       , io     = sails.io
 
     res.view()
-
-    setTimeout(function() {
-      io.sockets.emit( 'message', {
-        mobile: true,
-        status: 'Mobile client id ' + gameId + ' connected'
-      })
-    },
-      1000
-    )
-
 
   }
 
