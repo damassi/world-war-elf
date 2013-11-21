04e2dd9f73b2f554f832cd17c761a0f0d713d5bb
Christopher Pappas <damassi.pappas@gmail.com>
2013-11-20 21:54:25 -0800

    Switching to easel

--- client-side/javascripts/views/home/HomeView.js
+++ client-side/javascripts/views/home/HomeView.js
@@ -16,16 +16,11 @@ var HomeView = View.extend({
   initialize: function (options) {
     this._super(options)
 
-    this.placeHolder = new PIXI.Sprite( new PIXI.Texture.fromImage('/assets/images/placeholder-home.jpg'))
-
   },
 
 
   render: function (options) {
     this._super()
-
-    this.container.addChild( this.placeHolder )
-    this.stage.addChild( this.container )
 
     return this
   }
